f43b282d15d88df224587d1f11aca38f6946c03f
Nikhil <nikhil.raj@cgtindia.in>
2018-03-12 17:35:54 +0530

    changes

--- script/script.js
+++ script/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
+
     $.each($('div.original-price s'), function(index, item) {
-        console.log();
+
         var new_price;
         var original_price = item.innerHTML;
         var price = original_price.split("$");
         console.log(price);
         if (parseFloat(price[1]) > 20) {
             if (parseFloat(price[1]) < 100) {
+                item.setAttribute("class", "strike");
                 new_price = parseFloat(price[1]) * 0.8;
                 $(item.parentElement.parentElement).append('<div class="discount_price">$' + new_price.toFixed(2) + '</div>');
             }
